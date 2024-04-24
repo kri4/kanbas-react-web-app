f723cf993056e1a1cc50d29c187b7586abb514cc
kritika <103331480+kri4@users.noreply.github.com>
2024-04-24 16:39:17 -0700

    idk

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -17,8 +17,8 @@ import axios from "axios";
 
 function Courses({ courses }: { courses: any[]; }) {
     const { courseId } = useParams();
-    const COURSES_API = "https://kanbas-node-server-app-3-7335311db66e.herokuapp.com/";
-    // const COURSES_API = "http://localhost:4000/api/courses";
+    // const COURSES_API = "https://kanbas-node-server-app-3-7335311db66e.herokuapp.com/";
+    const COURSES_API = "http://localhost:4000/api/courses";
     const [course, setCourse] = useState<any>({ _id: "" });
     const findCourseById = async (courseId?: string) => {
       const response = await axios.get(
